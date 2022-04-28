33733107283697917b5a83e2b1d894670582a1b2
Eden Castillo Asport <35803612+phenomenone@users.noreply.github.com>
2022-04-28 18:52:49 -0500

    Completed.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const img1 =
+  "https://cheapandcheerfulcooking.com/wp-content/uploads/2021/01/chinese-fried-noodles-basic-recipe-1.jpg";
+const img2 =
+  "https://static.toiimg.com/thumb/52467119.cms?imgsize=1106358&width=800&height=800";
+const img3 =
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Mama_instant_noodle_block.jpg/1200px-Mama_instant_noodle_block.jpg";
+
 ReactDOM.render(
   <div>
-    <h1>My Favourite Foods</h1>
+    <h1 className="heading">My Favourite Foods</h1>
     <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
+      <img className="food-img" src={img1} alt="img1" />
+      <img className="food-img" src={img2} alt="img2" />
+      <img className="food-img" src={img3} alt="img3" />
     </ul>
   </div>,
   document.getElementById("root")
